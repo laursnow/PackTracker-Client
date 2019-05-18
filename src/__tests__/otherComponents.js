e76667ac5472c754ca56b3ad624d34d33e88b70b
Lauren Snow <laursnowie@gmail.com>
2019-05-18 01:21:10 -0400

    ??

--- src/__tests__/otherComponents.js
+++ src/__tests__/otherComponents.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import {About} from '../components/about';
 import {LandingPage} from '../components/landing-page';
 import {ErrorComponent} from '../components/error'; 
