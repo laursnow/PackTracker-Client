6cb59288a9605376ff6a4227c73768ee6a313475
Lauren Snow <laursnowie@gmail.com>
2019-05-24 22:05:07 -0400

    update server for live app - added an s

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 exports.API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://packtracker-server.herokuapp.com/api";
+  process.env.REACT_APP_API_BASE_URL || "https://packtracker-server.herokuapp.com/api";
 
 exports.AUTH_BASE_URL =
-  process.env.REACT_APP_AUTH_BASE_URL || "http://packtracker-server.herokuapp.com/auth";
+  process.env.REACT_APP_AUTH_BASE_URL || "https://packtracker-server.herokuapp.com/auth";
 
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:3000" || "https://packtracker-app.herokuapp.com";
